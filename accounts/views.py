7c23b995e277b73800475d3d211241e4a51c4c48
Nur Bengisu <bengisu.cam@gmail.com>
2020-05-16 17:40:23 +0300

    login.html error removed

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-<<<<<<< Updated upstream
-def cerate_account(request):
-=======
 def login(request):
->>>>>>> Stashed changes
     return render(request, 'accounts/login.html');
