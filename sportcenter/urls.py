1772157ee638636d15b9561bbfb9bf107c98f2c5
girayhanyldrm <girayhan2323@gmail.com>
2020-05-31 15:47:13 +0300

    room page and functions

--- sportcenter/urls.py
+++ sportcenter/urls.py
@@ -4,5 +4,10 @@ from . import views
 # seperate each app url individual files, dont overwhelm the WebProject/urls.py !
 
 urlpatterns = [
-    path('sportcenter/', views.sport_center_list)
+    path('<int:user_id>/rooms/', views.list_myrooms, name='list_myrooms'),
+    path('<int:user_id>/rooms/add/', views.add_room, name='add_room'),
+    path('<int:user_id>/rooms/create/', views.create_room, name='create_room'),
+    path('<int:user_id>/rooms/delete/<int:room_id>/', views.delete_room, name='delete_room'),
+    path('<int:user_id>/rooms/update/<int:room_id>/', views.update_room, name='update_room'),
+    path('<int:user_id>/rooms/update/action/<int:room_id>/', views.update_room_action, name='update_room_action')
 ]
--- sportcenter/views.py
+++ sportcenter/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
-
+from accounts.models import User
+from sportcenter.models import Room
 
 # Create your views here.
-def sport_center_list(request):
-    return render(request, 'sportcenter/sport_center_list.html')
+
+''' ROOM '''
+
+
+def list_myrooms(request, user_id):
+    active_user = User.objects.get(pk=user_id)
+    rooms = Room.objects.all().filter(sport_center_id_id=active_user.sport_center_id_id)
+    return render(request, 'sportcenter/list_myrooms.html', {'myrooms': rooms, 'active_user': active_user})
+
+
+def add_room(request, user_id):
+    active_user = User.objects.get(pk=user_id)
+    return render(request, 'sportcenter/add_room.html', {'active_user': active_user})
+
+
+def create_room(request, user_id):
+    active_user = User.objects.get(pk=user_id)
+    room_name = request.POST.get("room_name")
+    room_capacity = request.POST.get("room_capacity")
+    new_room = Room(room_name=room_name, room_capacity=room_capacity, sport_center_id_id=active_user.sport_center_id_id)
+    new_room.save()
+    return list_myrooms(request, user_id)
+
+
+def delete_room(request, user_id, room_id):
+    deleted_room = Room.objects.get(pk=room_id)
+    deleted_room.delete()
+    return list_myrooms(request, user_id)
+
+
+def update_room(request, user_id, room_id):
+    updated_room = Room.objects.get(pk=room_id)
+    active_user = User.objects.get(pk=user_id)
+    return render(request, 'sportcenter/update_room.html', {'active_user': active_user, 'update_room': updated_room})
+
+
+def update_room_action(request, user_id, room_id):
+    updated_room = Room.objects.get(pk=room_id)
+    updated_room.room_name = request.POST.get("room_name")
+    updated_room.room_capacity = request.POST.get("room_capacity")
+    updated_room.save()
+    return list_myrooms(request, user_id)
+
+
+''' INSTRUCTOR '''
